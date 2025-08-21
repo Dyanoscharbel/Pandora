c4e813632078208fc1e65cea1ee09969f75eab5e
SANNI Adéchinan Dyanos Charbel <135537656+Dyanoscharbel@users.noreply.github.com>
2025-08-21 17:54:49 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { plausible } from "@plausible-analytics/tracker";
+import plausible from "@plausible-analytics/tracker/dist/plausible";
 
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
